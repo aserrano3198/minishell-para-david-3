958fe9d99ad663909fee16e9be65c20ec395263e
Alvaro Serrano del Alamo <alamo3198@gmail.com>
2020-02-03 14:26:55 +0100

    echo completo

--- srcs/builtin_echo.c
+++ srcs/builtin_echo.c
@@ -4,16 +4,25 @@ int        builtin_echo(shell_t *shell)
 {
     int i;
     char *todisplay;
+    int flag;
 
     i = 1;
+    flag = 0;
+    if (ft_strncmp(shell->cmd_exec_parsed[1], "-n", 2) == 0)
+    {
+        i++;
+        flag = 1;
+    }
     while (shell->cmd_exec_parsed[i])
-        {
-            todisplay = ft_strtrim(shell->cmd_exec_parsed[i], "\"");
-            write (1, todisplay, ft_strlen(todisplay));
+    {
+        todisplay = ft_strtrim(shell->cmd_exec_parsed[i], "\"");
+        write (1, todisplay, ft_strlen(todisplay));
+        if (shell->cmd_exec_parsed[i + 1])
             write (1, " ", 1);
-            free(todisplay);
-            i++;
-        }
-    write(1, "\n", 1);
+        free(todisplay);
+        i++;
+    }
+    if (!flag)
+        write(1, "\n", 1);
     return (1);
 }
