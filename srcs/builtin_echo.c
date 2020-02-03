ff96ee9913f672cbac4602f7087f1c576ae3298c
Alvaro Serrano del Alamo <alamo3198@gmail.com>
2020-02-03 14:41:25 +0100

    echo completo con flag

--- srcs/builtin_echo.c
+++ srcs/builtin_echo.c
@@ -3,10 +3,11 @@
 int        builtin_echo(shell_t *shell)
 {
     int i;
-    char *todisplay;
+    int o;
     int flag;
 
     i = 1;
+    o = -1;
     flag = 0;
     if (ft_strncmp(shell->cmd_exec_parsed[1], "-n", 2) == 0)
     {
@@ -15,11 +16,11 @@ int        builtin_echo(shell_t *shell)
     }
     while (shell->cmd_exec_parsed[i])
     {
-        todisplay = ft_strtrim(shell->cmd_exec_parsed[i], "\"");
-        write (1, todisplay, ft_strlen(todisplay));
+        while (shell->cmd_exec_parsed[i][++o])
+            if (shell->cmd_exec_parsed[i][o] != '\"')
+                write (1, shell->cmd_exec_parsed[i] + o, 1);
         if (shell->cmd_exec_parsed[i + 1])
             write (1, " ", 1);
-        free(todisplay);
         i++;
     }
     if (!flag)
